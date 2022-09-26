4401826089b6a1573f062e52a8d03bd6754dd23d
dpffpsk <gpljw0419@gmail.com>
2022-09-26 23:09:46 +0900

    UI 그리기

---
+++ Diary/DiaryCell.swift
@@ -0,0 +1,12 @@
+//
+//  DiaryCell.swift
+//  Diary
+//
+//  Created by wony on 2022/09/26.
+//
+
+import UIKit
+
+class DiaryCell: UICollectionViewCell {
+    
+}
---
+++ Diary/StarCell.swift
@@ -0,0 +1,12 @@
+//
+//  StarCell.swift
+//  Diary
+//
+//  Created by wony on 2022/09/26.
+//
+
+import UIKit
+
+class StarCell: UICollectionViewCell {
+    
+}
